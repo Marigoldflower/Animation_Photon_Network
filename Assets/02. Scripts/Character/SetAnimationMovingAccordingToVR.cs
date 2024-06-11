957f772cdd060ebef7b05ed3f729b53ce8020b5d
Cid <prgmrJeong@gmail.com>
2024-06-11 15:04:30 +0900

    Foot Sync

--- Assets/02. Scripts/Character/SetAnimationMovingAccordingToVR.cs
+++ Assets/02. Scripts/Character/SetAnimationMovingAccordingToVR.cs
@@ -8,18 +8,23 @@ public class SetAnimationMovingAccordingToVR : MonoBehaviour
 {
     public InputActionProperty moveAction; // XR 컨트롤러의 스틱 입력을 받기 위한 프로퍼티
     private Animator animator;
+    private PhotonView pv;
 
     void Start()
     {
+        pv = this.GetComponent<PhotonView>();
         animator = this.GetComponent<Animator>();
     }
 
     void Update()
     {
-        Vector2 moveInput = moveAction.action.ReadValue<Vector2>(); // 스틱 입력 읽기
+        if (pv.IsMine)
+        {
+            Vector2 moveInput = moveAction.action.ReadValue<Vector2>(); // 스틱 입력 읽기
 
-        animator.SetFloat("MoveX", moveInput.x);
-        animator.SetFloat("MoveY", moveInput.y);
+            animator.SetFloat("MoveX", moveInput.x);
+            animator.SetFloat("MoveY", moveInput.y);            
+        }
     }
 
 }
