428c1327668799f8c2a5f68a12ced0616240d525
Chicken Villain <55979653+fnatm416@users.noreply.github.com>
2024-06-13 12:51:52 +0900

    feat: RPC Test2
    플레이어가 방에 입장하면 RPC를 보내도록 스크립트 수정

--- Assets/02. Scripts/Character/CharacterAvatar.cs
+++ Assets/02. Scripts/Character/CharacterAvatar.cs
@@ -14,11 +14,11 @@ namespace SCI
         public Transform lowerTf;
         public Transform footTf;
 
-        private GameObject currentHead;
-        private GameObject currentFace;
-        private GameObject currentUpper;
-        private GameObject currentLower;
-        private GameObject currentFoot;
+        public int currentHead { get; private set; }
+        public int currentFace { get; private set; }
+        public int currentUpper { get; private set; }
+        public int currentLower { get; private set; }
+        public int currentFoot { get; private set; }
 
         public void Initialize()
         {
@@ -27,39 +27,39 @@ namespace SCI
             {
                 // enum타입을 순회하여 각 타입별로 코스튬 착용
                 var type = (CostumeType)Enum.ToObject(typeof(CostumeType), i);
-                SwitchCostume(type);
+                SetCostume(type, DataManager.Instance.GetCostumeData(type));
             }
         }
 
-        public void SwitchCostume(CostumeType type)
+        public void SetCostume(CostumeType type, int index)
         {
             // 코스튬 변경
             switch (type)
             {
                 case CostumeType.Head:
                     UnDressCostume(CostumeType.Head);
-                    DressCostume(CostumeType.Head);
+                    DressCostume(CostumeType.Head, index);
                     break;
                 case CostumeType.Face:
                     UnDressCostume(CostumeType.Face);
-                    DressCostume(CostumeType.Face);
+                    DressCostume(CostumeType.Face, index);
                     break;
                 case CostumeType.Upper:
                     UnDressCostume(CostumeType.Upper);
-                    DressCostume(CostumeType.Upper);
+                    DressCostume(CostumeType.Upper, index);
                     break;
                 case CostumeType.Lower:
                     UnDressCostume(CostumeType.Lower);
-                    DressCostume(CostumeType.Lower);
+                    DressCostume(CostumeType.Lower, index);
                     break;
                 case CostumeType.Foot:
                     UnDressCostume(CostumeType.Foot);
-                    DressCostume(CostumeType.Foot);
+                    DressCostume(CostumeType.Foot, index);
                     break;
             }
         }
 
-        private void DressCostume(CostumeType type)
+        private void DressCostume(CostumeType type, int index)
         {
             var instance = DataManager.Instance;
 
@@ -70,36 +70,36 @@ namespace SCI
                 case CostumeType.Head:
                     if (headTf.childCount > instance.GetCostumeData(CostumeType.Head))
                     {
-                        currentHead = headTf.GetChild(instance.GetCostumeData(CostumeType.Head)).gameObject;
-                        currentHead.SetActive(true);
+                        currentHead = index;
+                        headTf.GetChild(index).gameObject.SetActive(true);
                     }
                     break;
                 case CostumeType.Face:
                     if (faceTf.childCount > instance.GetCostumeData(CostumeType.Face))
                     {
-                        currentFace = faceTf.GetChild(instance.GetCostumeData(CostumeType.Face)).gameObject;
-                        currentFace.SetActive(true);
+                        currentFace = index;
+                        faceTf.GetChild(index).gameObject.SetActive(true);
                     }
                     break;
                 case CostumeType.Upper:
                     if (upperTf.childCount > instance.GetCostumeData(CostumeType.Upper))
                     {
-                        currentUpper = upperTf.GetChild(instance.GetCostumeData(CostumeType.Upper)).gameObject;
-                        currentUpper.SetActive(true);
+                        currentUpper = index;
+                        upperTf.GetChild(index).gameObject.SetActive(true);
                     }
                     break;
                 case CostumeType.Lower:
                     if (lowerTf.childCount > instance.GetCostumeData(CostumeType.Lower))
                     {
-                        currentLower = lowerTf.GetChild(instance.GetCostumeData(CostumeType.Lower)).gameObject;
-                        currentLower.SetActive(true);
+                        currentLower = index;
+                        lowerTf.GetChild(index).gameObject.SetActive(true);
                     }
                     break;
                 case CostumeType.Foot:
                     if (footTf.childCount > instance.GetCostumeData(CostumeType.Foot))
                     {
-                        currentFoot = footTf.GetChild(instance.GetCostumeData(CostumeType.Foot)).gameObject;
-                        currentFoot.SetActive(true);
+                        currentFoot = index;
+                        footTf.GetChild(index).gameObject.SetActive(true);
                     }
                     break;
             }
---
+++ Assets/02. Scripts/InGame/InGameManager.cs
@@ -0,0 +1,96 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SCI
+{
+    public class InGameManager : MonoBehaviourPunCallbacks
+    {
+        void Awake()
+        {
+            // 스크린의 사이즈 조정 
+            Screen.SetResolution(1080, 720, false);
+
+            // 높을 수록 네트워크 전송 속도가 빠르다. 대신 과부하가 걸릴 확률이 높음
+            PhotonNetwork.SendRate = 60;
+            PhotonNetwork.SerializationRate = 30;
+            PhotonNetwork.GameVersion = "1";
+        }
+
+        void Start()
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("네트워크 마스터에 접속");
+
+            RoomOptions roomOptions = new RoomOptions();
+            roomOptions.MaxPlayers = 2; // 최대 플레이어 수를 2로 설정
+            PhotonNetwork.JoinOrCreateRoom("GameRoom", roomOptions, null);
+        }
+
+        public override void OnPlayerEnteredRoom(Player newPlayer)
+        {
+            base.OnPlayerEnteredRoom(newPlayer);
+            Debug.Log("New Player Come In");
+
+            // RPC를 보내서 자신의 코스튬 정보를 전송
+            var player = GetLocalPlayer();
+            if (player != null)
+            {
+                var avatar = player.GetComponent<CharacterAvatar>();
+                photonView.RPC("SyncCostume", RpcTarget.Others, avatar, DataManager.Instance.playerData.costumeDatas);
+            }
+            //////////////////////////////
+        }
+
+        public override void OnJoinedRoom()
+        {
+            if (PhotonNetwork.IsConnectedAndReady)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-1f, 1f), 1f, Random.Range(-1f, 1f));
+                PhotonNetwork.Instantiate("Character2", spawnPosition, Quaternion.identity);
+
+                // 로컬 플레이어 코스튬 갱신
+                var player = GetLocalPlayer();
+                if (player != null)
+                {
+                    var avatar = player.GetComponent<CharacterAvatar>();
+                    SyncCostume(avatar, DataManager.Instance.playerData.costumeDatas);
+                }
+                //////////////////////////////
+            }
+        }
+
+        private NetworkCharacterController GetLocalPlayer()
+        {
+            var players = FindObjectsOfType<NetworkCharacterController>();
+
+            if (players == null)
+                return null;
+
+            foreach (var player in players)
+            {
+                if (player.GetComponent<PhotonView>().IsMine)
+                {
+                    return player;
+                }
+            }
+
+            return null;
+        }
+
+        [PunRPC]
+        public void SyncCostume(CharacterAvatar avatar, Dictionary<CostumeType, int> datas)
+        {
+            foreach (var data in datas)
+            {
+                avatar.SetCostume(data.Key, data.Value);
+            }
+        }
+    }
+}
